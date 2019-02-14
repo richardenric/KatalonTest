597fe7533f816bab842b63d841ed42497a8eb06e
Experis1 <Experis1@PCFMAD452.gesif.local>
2019-02-14 10:10:50 +0100

    He realizado algunos cambios

---
+++ Test Suites/US-Prueba1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>US-Prueba1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4e2d8ab0-3a98-4d96-b1a5-fd3f35d83d0a</testSuiteGuid>
+</TestSuiteEntity>
